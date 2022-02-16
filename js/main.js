2a3df17cb7bae2f70ddcfe9c8f951392353f73bb
Tuan Tuan <nguyenvantuan09031999mdc@gmail.com>
2022-02-16 17:37:19 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import {
+  getCellElementList,
+  getCurrentTurnElement,
+  getCellElementAtIdx,
+  getGameStatusElement,
+} from "./selectors.js";
+
+// console.log(getCellElementList());
+// console.log(getCurrentTurnElement());
+// console.log(getCellElementAtIdx(4));
+// console.log(getGameStatusElement());
+/**
+ * Global variables
+ */
+let currentTurn = "cross";
+let isGameEnded = false;
+let cellValues = new Array(9).fill("");
+
+/**
+ * TODOs
+ *
+ * 1. Bind click event for all cells
+ * 2. On cell click, do the following:
+ *    - Toggle current turn
+ *    - Mark current turn to the selected cell
+ *    - Check game state: win, ended or playing
+ *    - If game is win, highlight win cells
+ *    - Not allow to re-click the cell having value.
+ *
+ * 3. If game is win or ended --> show replay button.
+ * 4. On replay button click --> reset game to play again.
+ *
+ */
